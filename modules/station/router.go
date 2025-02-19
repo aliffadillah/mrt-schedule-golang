2a6d55f003e72cf67790bfaee0b26415b695713c
Muhammad Alif Fadillah <ed.aliffadillah@gmail.com>
2025-02-19 17:16:36 +0700

    Common, Layer (Stations Name)

--- modules/station/router.go
+++ modules/station/router.go
@@ -1,10 +1,37 @@
 package station
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/aliffadillah/mrt-schedule-golang.git/common/response"
+	"github.com/gin-gonic/gin"
+)
 
 func Initiate(router *gin.RouterGroup) {
-	station := router.Group("/station")
+	stationService := NewService()
+	station := router.Group("/stations")
 	station.GET("", func(c *gin.Context) {
 		//code service
+		GetAllStation(c, stationService)
 	})
+}
+
+func GetAllStation(c *gin.Context, service Service) {
+	datas, err := service.GetAllStations()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.APIResponse{
+			Success: false,
+			Message: err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+	c.JSON(
+		http.StatusOK,
+		response.APIResponse{
+			Success: true,
+			Message: "Successfully get all station",
+			Data:    datas,
+		},
+	)
 }
